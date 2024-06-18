388d93b2a4ae7707ca9dd48bd973faecf94ac614
Vaibhav <sundriyal.vaibhav11@gmail.com>
2024-06-19 03:20:31 +0530

    Blogify

--- backend/Server/app.js
+++ backend/Server/app.js
@@ -16,11 +16,8 @@ app.get('/ip', (request, response) => response.send(request.ip));
 app.get('/x-forwarded-for', (request, response) => response.send(request.headers['x-forwarded-for']));
 app.use(cors());
 app.use(express.json());
-const NUM_INSTANCES = 5;
-const START_PORT = 8000;
 
 
-//static files folders
 connectDB();
 dotenv.config();
 
@@ -33,11 +30,6 @@ app.post("/authenticated",authenticateToken,(req,res)=>{
 })
 
 
-
-// Serve static files from the 'public/scripts' directory
-
-// app.use(express.static("public/views"));
-
 app.use(helmet.noSniff());
 app.use(helmet.referrerPolicy({ policy: "no-referrer" }));
 
@@ -47,27 +39,10 @@ app.use(postsRoute);
 app.use(logoutRoute);
 
 function startServers() {
-    // for (let i = 0; i < NUM_INSTANCES; i++) {
-    //      const port = START_PORT + i;
     const port = 8000;
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
-    //  }
 }
 startServers();
-//  if (process.env.NODE_APP_INSTANCE !== undefined) {
-//      startServers();
-//  } else {
-//      const instances = NUM_INSTANCES || 3;
-//     const { fork } = await import('pm2');
-//      fork(import.meta.url, {
-//          instances,
-//          exec_mode: 'cluster'
-//      }, (err) => {
-//          if (err) {
-//              console.error('Error starting application:', err);
-//              process.exit(1);
-//          }
-//      });
-//  }
+
